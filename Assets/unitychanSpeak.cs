c7ef362bc2d3a9fb2ee551b837049314999bde5a
moririring <moriri.ring10@gmail.com>
2014-07-11 16:59:30 +0900

    音

---
+++ Assets/unitychanSpeak.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class unitychanSpeak : MonoBehaviour {
+
+	public AudioClip selectSound;
+
+	// Use this for initialization
+	void Start () {
+		audio.clip = selectSound;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Fire1")){
+			audio.Play();
+		} 
+	}
+}
